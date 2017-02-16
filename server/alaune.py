21ba5c4987289c522df80cb0a7741908a9168a13
NicoVendrick <nico.24190@gmail.com>
2017-02-16 13:28:24 +0100

    Ajout dans alaune.py du journal « lePoint » et « journal du net » ainsi que la fonction htmlize2 qui permet d’avoir des url complet donc non relatif.

--- server/alaune.py
+++ server/alaune.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, request
 
-import CourrierInternational
+import CourrierInternational, LePoint, JournalduNet
 
 app = Flask('A la une')
 
@@ -17,6 +17,8 @@ def index():
 	page_content += '<option value="liberation">Libération</option>'
 	page_content += '<option value="lemonde">LeMonde</option>'
 	page_content += '<option value="figaro">Le Figaro</option>'
+	page_content += '<option value="lejournaldunet">Le Journal du Net</option>'
+	page_content += '<option value="lepoint">Le Point</option>'
 	page_content += '<option value="courrier">Courrier International</option>'
 	page_content += '</select>'
 
@@ -31,6 +33,14 @@ def quel_journal():
 		targetURL = 'http://www.courrierinternational.com'
 		titres = CourrierInternational.unes(targetURL)
 		return htmlize(titres, targetURL)
+	elif journal == 'lepoint':
+		targetURL = 'http://www.lepoint.fr'
+		titres = LePoint.unes(targetURL)
+		return htmlize(titres, targetURL)
+	elif journal == 'lejournaldunet':
+		targetURL = 'http://www.lejournaldunet.fr'
+		titres = JournalduNet.unes(targetURL)
+		return htmlize2(titres, targetURL)
 	else:
 		return journal
 
@@ -40,6 +50,15 @@ def htmlize(titles_and_href, targetURL):
 		html += '<h2>'
 		html += '<a href="' + targetURL + item[1] + '">' + item[0].strip() + '</a></h2>\n'
 	return html
+
+#### htmlize2 à la même fonction que htmlize mais sans répétition de targetURL
+def htmlize2(titles_and_href, targetURL):
+	html = ''
+	for item in titles_and_href:
+		html += '<h2>'
+		html += '<a href="' + item[1] + '">' + item[0].strip() + '</a></h2>\n'
+	return html
+####
 
 if __name__ == '__main__':
 	app.run(debug=True)
